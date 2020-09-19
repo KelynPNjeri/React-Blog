4d91058671c1229112625ba392787933a3fdb730
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-09-20 01:05:08 +0300

    Create Homepage and add routing functionality.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello Welcome to my Blog</h1>
+    <p>
+        Nam blandit maximus ultricies. Duis faucibus ipsum nec nulla sodales ornare. Suspendisse non magna justo. Sed neque tortor, posuere accumsan aliquet eu, laoreet et dolor. Curabitur libero ante, eleifend vitae varius vitae, euismod eget tortor. Mauris eros tellus, pretium ornare congue non, consectetur ac arcu. Nunc a velit tristique, luctus purus eget, tempus leo. Curabitur eu justo id sem pulvinar facilisis.
+    </p>
+    <p>
+        Aenean molestie eget sem eu viverra. Duis egestas rhoncus velit in feugiat. Praesent eu blandit tortor. Suspendisse eget molestie risus, sit amet lobortis dolor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris venenatis tempus lectus, vitae eleifend urna. Cras sed mi nec nisi semper bibendum ut vitae eros. Vivamus eleifend orci diam. Nunc nulla mi, tempus vel ex a, egestas tristique purus. Donec viverra in nunc nec iaculis. In vel suscipit orci. Nullam eu nisi quis nunc hendrerit auctor in et diam. Sed semper nisi mollis feugiat lobortis. Donec bibendum lacus congue orci pretium egestas. Praesent molestie justo orci, in molestie nisi mattis et.
+    </p>
+    <p>
+        Maecenas mollis tellus quis dolor ultricies, a lobortis justo tincidunt. Morbi pretium venenatis euismod. Donec quis mi felis. In hac habitasse platea dictumst. Donec id orci ac eros sagittis viverra quis vitae mi. Duis eros mauris, pulvinar non libero quis, interdum dignissim felis. Cras pretium id nunc quis euismod. Nulla accumsan risus vitae mauris faucibus dignissim. Pellentesque imperdiet dolor nec ante malesuada, vel interdum massa maximus. Duis pellentesque massa eget quam iaculis, vel dapibus turpis efficitur. Nunc diam urna, aliquam id pharetra a, tempus quis nisi.
+    </p>
+    </>
+);
+
+export default HomePage;
